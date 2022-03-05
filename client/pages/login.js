f006d99271e22a5b7a91621dd6f59eb3eaeee485
Arushi Gandhi <Arushi.gandhi@gmail.com>
2022-03-05 11:56:33 +0530

    had started login

---
+++ client/pages/login.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../styles/Login.module.scss";
+
+export default function login() {
+  return (
+    <>
+      <div className={styles.loginContainer}><div>hey</div></div>
+    </>
+  );
+}
