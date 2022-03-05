41a10a6c09a986482f17e454547a97ffcbe7305c
S Sitaraman <sitaraman.raghav.india@gmail.com>
2022-03-05 13:06:14 +0530

    Landing

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,38 @@
-import styles from '../styles/Home.module.scss'
-
+import Navbar from "components/Navbar";
+import styles from "../styles/Home.module.scss";
+import { Carousel } from "antd";
+const contentStyle = {
+  height: "160px",
+  color: "#fff",
+  lineHeight: "160px",
+  textAlign: "center",
+  background: "#364d79",
+};
 export default function Home() {
   return (
-    <div className={styles.container}>
-      we gotta win yall
-    </div>
-  )
+    <>
+      <Navbar />
+      <div className={styles.container}>
+        <div className={styles.LeftSection}>
+          Now buy and sell handcrafted items from any corner of India.
+        </div>
+        <div className={styles.RightSection}>
+          {/* <Carousel effect="fade" autoplay autoplaySpeed={1800}>
+            <div>
+              <h3 style={contentStyle}>1</h3>
+            </div>
+            <div>
+              <h3 style={contentStyle}>2</h3>
+            </div>
+            <div>
+              <h3 style={contentStyle}>3</h3>
+            </div>
+            <div>
+              <h3 style={contentStyle}>4</h3>
+            </div>
+          </Carousel> */}
+        </div>
+      </div>
+    </>
+  );
 }
